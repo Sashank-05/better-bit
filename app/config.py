41ea6d519553f71ae320b12b3af26c113d6d2694
Sashank-05 <sashankvanka@outlook.com>
2024-03-24 11:25:20 +0530

    a

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY',"emergency-password-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- app/routes.py
+++ app/routes.py
@@ -10,19 +10,23 @@ branches = json.loads(open('app/db/branches.json').read())
 
 @app.route('/')
 def index():
+    global branches
     return render_template('index.html', branches=branches['branches'])
 
 
 @app.route('/<coursecode>/semesters')
 def semesters(coursecode):
+    global branches
     print(branches[coursecode])
     return render_template('sems.html', sems=branches[coursecode])
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin_panel():
+    global branches
     if request.method == 'POST':
         sql_command = request.form['sql_command']
+        branches = json.loads(open('app/db/branches.json').read())
         # result = execute_sql(sql_command)
         # return render_template('admin.html', result=result)
 
