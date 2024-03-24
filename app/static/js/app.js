a51f5bc8fa0756d632a08a04dbfb0957d07fdc85
pranav244872 <aekbotepranav25@gmail.com>
2024-03-24 22:41:25 +0530

    did smtg

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,6 @@
 const bg = "#21063a";
 const fg = "#c8bbf0";
+const bgspcl = "#0d0217";
 const cursor = "#c8bbf0";
 const color0 = "#21063a";
 const color1 = "#6e48db";
@@ -18,18 +19,23 @@ const color13 = "#9a5ddc";
 const color14 = "#c667e3";
 const color15 = "#c8bbf0";
 
-if (screen.width == 360) {
-  //nav element
-  //changing the #logo size
-  var nav = document.querySelector("nav");
-  var logo = document.querySelectorAll("#logo");
-  logo.forEach(function (logo) {
-    logo.style.fontSize = "15px";
-  });
-  //changing padding of nav bar
-
+if (screen.width >= 360) {
   var navbar = document.getElementById("navbar");
-  navbar.innerHTML = `<a href="#"><span class="material-icons" style="color: ${fg}; height: 15px;">menu</span></a>`;
-  navbar.style.width = "10%";
+  navbar.innerHTML = `<a href="#"><span class="material-icons" id="menu" style="color: ${fg}; height: 10px;">menu</span></a>`;
+  //menu bar features
+  document.querySelector("#menu").addEventListener("click", menubaropen);
+  var menubar = false;
+  function menubaropen() {
+    //created a div for pop-in menu
+    const newDiv = document.createElement("div");
+    //innerhtml for the pop-in menu
+    newDiv.innerHTML =
+      '<a href="#" id="back" style="margin: 0px 0px 0px 200px"><span class="material-icons" style="height: 10px;">arrow_forward</span></a><a href="#">Contributors</a><a href="#">Contribute</a><a href="#">Login</a>';
+    //styling
+    newDiv.style.cssText = `position: fixed; top: 0; right: 0; height: 100vh; width: 250px; background-color: ${bgspcl}; backdrop-filter: blur(20px); opacity: 1; display: flex; flex-direction: column; border-left: solid; border-color: ${color1};`;
+    newDiv.querySelectorAll("a").forEach((element) => {
+      element.style.cssText = `padding: 15px 10px; color: ${fg}; text-decoration: none;`;
+    });
+    document.body.appendChild(newDiv);
+  }
 }
-
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,31 +1,33 @@
 function selectCourse(courseCode, courseName) {
-    localStorage.setItem("selectedCourseCode", courseCode);
-    localStorage.setItem("selectedCourseName", courseName);
+  localStorage.setItem("selectedCourseCode", courseCode);
+  localStorage.setItem("selectedCourseName", courseName);
 
-    var nxtPg = "/" + courseCode + "/semesters";
-    smoothChange(nxtPg);
+  var nxtPg = "/" + courseCode + "/semesters";
+  smoothChange(nxtPg);
 }
 
 function updatePageContent(content) {
-    document.open();
-    document.write(content);
-    document.close();
+  document.open();
+  document.write(content);
+  document.close();
 }
-
 
 function goBack() {
-    window.history.back();
+  window.history.back();
 }
 
-function smoothChange(url){
-    var nxtPg = url;
-    var nxtpgReq = new XMLHttpRequest();
-    nxtpgReq.open('GET', nxtPg, true);
-    nxtpgReq.onreadystatechange = function() {
-        if (nxtpgReq.readyState === XMLHttpRequest.DONE && nxtpgReq.status === 200) {
-            updatePageContent(nxtpgReq.responseText);
-            history.pushState({path: nxtPg}, '', nxtPg);
-        }
-    };
-    nxtpgReq.send();
+function smoothChange(url) {
+  var nxtPg = url;
+  var nxtpgReq = new XMLHttpRequest();
+  nxtpgReq.open("GET", nxtPg, true);
+  nxtpgReq.onreadystatechange = function () {
+    if (
+      nxtpgReq.readyState === XMLHttpRequest.DONE &&
+      nxtpgReq.status === 200
+    ) {
+      updatePageContent(nxtpgReq.responseText);
+      history.pushState({ path: nxtPg }, "", nxtPg);
+    }
+  };
+  nxtpgReq.send();
 }
