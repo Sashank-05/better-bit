b68a37d0c1baf288ac751f8e9b9343024d5ca2ea
Sashank-05 <sashankvanka@outlook.com>
2024-03-31 15:43:09 +0530

    latest @nodejs

--- app/mongocontroller.js
+++ app/mongocontroller.js
@@ -1,13 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 
-// Connect to MongoDB
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     if (err) throw err;
 
     const db = client.db('mydb');
     const collection = db.collection('mycollection');
 
-    // Export data from MongoDB
     collection.find().toArray((err, data) => {
         if (err) throw err;
 
