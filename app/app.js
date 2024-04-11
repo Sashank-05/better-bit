5322063ee9221d2ec0dfd19777332fcbc6b68c1f
pranav244872 <aekbotepranav25@gmail.com>
2024-04-11 11:28:22 +0530

    shifted to mongodb

--- app/app.js
+++ app/app.js
@@ -9,6 +9,20 @@ const imgs = fs.readdirSync("static/images")
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
+async function findSemesterByNumber(coursecode, sem) {
+  const branch = await Branch.findOne({
+    code: coursecode,  // Find branch by code
+    semesters: { $elemMatch: { number: sem } } // Search within semesters array
+  });
+
+  if (branch) {
+    const semester = branch.semesters.find(semester => semester.number === sem);
+    return semester; // Return the matching semester
+  } else {
+    return null; // Return null if branch or semester not found
+  }
+}
+
 const { connection, Branch } = require('./db.js');
 connection.once("open", () => {
   console.log("Connected to mongodb via db.js");
@@ -17,19 +31,36 @@ connection.once("open", () => {
   app.get('/', (req, res) => {
     Branch.find()
       .then((branches) => {
-        res.render('index', { branches: branches, imgs: imgs });
+        res.render('index', { parameter: "branches", boxes: branches, imgs: imgs }); //boxes = thing to be dynamically rendered
       });
   });
 
-  app.get('/:coursecode/semesters', (req, res) => {
+  app.get('/:coursecode', (req, res) => {
     const { coursecode } = req.params;
-    res.render('sems', { sems: branches[coursecode] });
+    Branch.findOne({ code: coursecode })
+      .then((result) => {
+        res.render('index', { parameter: "semesters", boxes: result, imgs: imgs });
+      });
   });
 
-  app.get('/:coursecode/semesters/:semester', (req, res) => {
-    res.send('Not implemented');
+  app.get('/:coursecode/:sem', (req, res) => {
+    const { coursecode } = req.params;
+    const { sem } = req.params;
+    Branch.findOne({ code: coursecode, semesters: { $elemMatch: { number: sem } } }, { "semesters.subjects": 1, "_id": 0 })
+      .then((result) => {
+        res.render('index', { parameter: "subjects", branch: coursecode, semNo: sem, boxes: result.semesters[0].subjects, imgs: imgs });
+      })
   });
 
+  app.get('/:coursecode/:sem/:sub', (req, res) => {
+    const { coursecode } = req.params;
+    const { sem } = req.params;
+    const { sub } = req.params;
+    Branch.findOne({ code: coursecode, semesters: { $elemMatch: { number: sem } } }, { "semesters.subjects": 1, "_id": 0 })
+      .then((result) => {
+        res.render('index', { parameter: "subjects", branch: coursecode, semNo: sem, boxes: result.semesters[0].subjects, imgs: imgs });
+      })
+  });
   app.get('/login', (req, res) => {
     res.render('login');
   });
